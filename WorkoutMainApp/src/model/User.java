763b59da7ccab222181cefed0ff865285a2f6a98
hayman347 <hayman4447@gmail.com>
2026-04-23 23:14:17 -0400

    Added new panels and structure

--- WorkoutMainApp/src/model/User.java
+++ WorkoutMainApp/src/model/User.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package workoutmainapp;
+package model;
 
 /**
  *
  * @author haydnmaneval
  */
-public class test {
-    /// this is a test to add
+public class User {
+    
 }
